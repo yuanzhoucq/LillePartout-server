3c4d7bb9b12b05f5628b7d3c593d1f0ca6308cb8
yuanzhoucq <yuanzhou.cq@gmail.com>
2017-09-02 15:39:38 +0200

    测试腾讯云 api

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php
-echo "hello world";
+require('./include.php');
+
+use QCloud\Cos\Api;
+
+$bucket = 'lillepartout';
+$src = './hello.txt';
+$dst = '/testfolder/hello.txt';
+$folder = '/testfolder';
+
+$app_id = getenv(APPSETTING_APPID);
+$secret_id = getenv(APPSETTING_SECRETID);
+$secret_key = getenv(APPSETTING_SECRETKEY);
+
+
+$config = array(
+    'app_id' => $app_id,
+    'secret_id' => $secret_id,
+    'secret_key' => $secret_key,
+    'region' => 'sgp',   // bucket所属地域：华北 'tj' 华东 'sh' 华南 'gz'
+    'timeout' => 60
+);
+
+
+date_default_timezone_set('PRC');
+$cosApi = new Api($config);
+
+$ret = $cosApi->createFolder($bucket, $folder);
+var_dump($ret);
+
 
 
 //获取 Azure 环境变量方式
