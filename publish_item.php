c00fe355eff44fdf8b071ab89da24191b8d13713
yuanzhoucq <yuanzhou.cq@gmail.com>
2017-09-02 18:36:26 +0200

    dev

--- publish_item.php
+++ publish_item.php
@@ -1,14 +1,14 @@
 <?php
 require('./sql.php');
-echo mysqli_get_host_info($link);
 
 $name = $_GET['name'];
 $price = $_GET['price'];
 $openid = $_GET['userId'];
 $des = $_GET['des'];
 
-$sql = "INSERT INTO `sellitems`( `userId`, `name`, `price`, `description`) VALUES ($openid, $name, $price, $des)";
+$sql = "INSERT INTO `sellitems`( `userId`, `name`, `price`, `description`) VALUES ($openid, $name, $price, $des);";
 $res = mysqli_query($link, $sql);
+echo mysqli_errno($link);
 mysqli_close($link);
 if($res) {
     echo 'ok';
