43307e29d118dda28331fedfca09c058e98d7de0
yuanzhoucq <yuanzhou.cq@gmail.com>
2017-09-02 18:33:58 +0200

    dev

--- publish_item.php
+++ publish_item.php
@@ -5,11 +5,11 @@ echo mysqli_get_host_info($link);
 $name = $_GET['name'];
 $price = $_GET['price'];
 $openid = $_GET['userId'];
-$des = '';
+$des = $_GET['des'];
 
 $sql = "INSERT INTO `sellitems`( `userId`, `name`, `price`, `description`) VALUES ($openid, $name, $price, $des)";
 $res = mysqli_query($link, $sql);
-
+mysqli_close($link);
 if($res) {
     echo 'ok';
 }
