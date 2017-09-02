2160c6431552909ad8c5e44fa37b30bc61038129
yuanzhoucq <yuanzhou.cq@gmail.com>
2017-09-02 23:30:13 +0200

    只显示未卖出物品

--- get_items.php
+++ get_items.php
@@ -1,6 +1,6 @@
 <?php
 require('./sql.php');
 
-$sql = "SELECT * FROM `sellitems`";
+$sql = "SELECT * FROM `sellitems` WHERE `sold`='0'";
 $res = mysqli_query($link, $sql)->fetch_all(MYSQLI_ASSOC);
 echo json_encode($res);
