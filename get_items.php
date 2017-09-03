058b7eec989b5559e0046add0519e741ae3dfd28
yuanzhoucq <yuanzhou.cq@gmail.com>
2017-09-03 12:08:27 +0200

    买东西页面逆序

--- get_items.php
+++ get_items.php
@@ -3,4 +3,5 @@ require('./sql.php');
 
 $sql = "SELECT * FROM `sellitems` WHERE `sold`='0'";
 $res = mysqli_query($link, $sql)->fetch_all(MYSQLI_ASSOC);
+$res = array_reverse($res);
 echo json_encode($res);
